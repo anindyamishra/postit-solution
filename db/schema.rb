3381a562a2e2e748ef7f33c680f473a7d80b1c6b
anindyamishra <anindyamishra2008@gmail.com>
2014-09-05 15:09:12 +0530

    Assignment: M:M between Post and Category

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140904102225) do
+ActiveRecord::Schema.define(version: 20140905092352) do
 
   create_table "categories", force: true do |t|
     t.string "name"
@@ -23,11 +23,19 @@ ActiveRecord::Schema.define(version: 20140904102225) do
     t.integer "post_id"
   end
 
+  create_table "post_categories", force: true do |t|
+    t.integer "post_id"
+    t.integer "category_id"
+  end
+
   create_table "posts", force: true do |t|
     t.string  "url"
     t.string  "title"
     t.text    "description"
     t.integer "user_id"
+  end
+
+  create_table "testing", force: true do |t|
   end
 
   create_table "users", force: true do |t|
